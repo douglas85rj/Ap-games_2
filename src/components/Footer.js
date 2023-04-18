f2a56e5370d63838a022eec02b558040d9391ff0
douglas85rj <douglas85rj@gmail.com>
2023-04-17 21:38:35 -0300

    Dellete_ApCSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 
 import styled from "styled-components";
-//import "../App.css";
+
 
 
 const Footer = () => {
--- src/index.js
+++ src/index.js
@@ -1,45 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Contato from './routes/Contato';
-import Home from './routes/Home';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Contato from "./routes/Contato";
+import Home from "./routes/Home";
 
 //Router
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter ([
-{
-path:"/",
-element:<App />,
-children: [
- 
+const router = createBrowserRouter([
   {
     path: "/",
-    element:<Home />,
-    },
-  
-  {
-    path: "contato",
-    element:<Contato />,
-    },
-  
-]
-},
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
 
+      {
+        path: "contato",
+        element: <Contato />,
+      },
+    ],
+  },
 ]);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    
     <RouterProvider router={router} />
-
- 
-
   </React.StrictMode>
 );
 
