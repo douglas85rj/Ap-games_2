1ac8d34fbf34bd3a31c3ebd1a845411117cc3957
douglas85rj <douglas85rj@gmail.com>
2023-04-18 22:23:17 -0300

    testes

--- src/components/tests/form.spec.js
+++ src/components/tests/form.spec.js
@@ -1,14 +1,14 @@
 import Form from '../Form'
 import {render, screen} from '@testing-library/react'
 
-test("validar valores do placeholder",() => { 
+test("validar valores do label",() => { 
 
 render(<Form />)
-const inputName = screen.getByPlaceholderText('Nome')
-const inputEmail = screen.getByPlaceholderText('email@seuemail.com')
+const inputName = screen.getByDisplayValue('firstName')
+const inputSobrenome = screen.getByDisplayValue('lastName')
 
-expect(inputName).toHaveAttribute("placeholder", "Nome")
-expect(inputEmail).toHaveAttribute("placeholder", "email@seuemail.com")
+expect(inputName).toHaveAttribute("input", "firstName")
+expect(inputSobrenome).toHaveAttribute("input", "lastName")
 
 
 })
